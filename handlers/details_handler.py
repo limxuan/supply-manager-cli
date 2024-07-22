c65c7128643ee58fd90d0501390515b978dc2a96
reconlx <reconlx@protonmail.com>
2024-07-22 13:26:30 +0800

    better data example

--- handlers/details_handler.py
+++ handlers/details_handler.py
@@ -1,27 +1,30 @@
 from tabulate import tabulate
 
 from handlers.continue_handler import continue_handler
-from managers.hospital_manager import (retrieve_hospital,
-                                       retrieve_hospital_codes,
-                                       update_hospital_data)
-from managers.supplier_manager import (get_supplier_info,
-                                       retrieve_supplier_codes,
-                                       update_supplier_data)
+from managers.hospital_manager import (
+    retrieve_hospital,
+    retrieve_hospital_codes,
+    update_hospital_data,
+)
+from managers.supplier_manager import (
+    get_supplier_info,
+    retrieve_supplier_codes,
+    update_supplier_data,
+)
 from utils.cli import clear_screen, go_back, select_from_list
-from utils.text_manipulation import (convert_to_snake_case,
-                                     get_between_parantheses)
+from utils.text_manipulation import convert_to_snake_case, get_between_parantheses
 from utils.textfiles_database import load_data
 
 
 def details_handler(controller):
-    options = ["Update Details", "View Details"]
+    options = ["View Details", "Update Details"]
     option = select_from_list("What action do you wish to perform?", options)
     if option == "Back":
         return go_back(controller)
     if option == options[0]:
-        update_details_handler(controller)
-    else:
         view_details_handler(controller)
+    else:
+        update_details_handler(controller)
     continue_handler(controller)
 
 
