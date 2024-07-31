820e9eed621910e9e9d061ce259ad20eb09bb9b6
reconlx <reconlx@protonmail.com>
2024-07-31 21:54:41 +0800

    fix(handlers/details_handler): suppliers are miswritten as hospitals

--- handlers/details_handler.py
+++ handlers/details_handler.py
@@ -1,18 +1,15 @@
 from tabulate import tabulate
 
 from handlers.continue_handler import continue_handler
-from managers.hospital_manager import (
-    retrieve_hospital,
-    retrieve_hospital_codes,
-    update_hospital_data,
-)
-from managers.supplier_manager import (
-    get_supplier_info,
-    retrieve_supplier_codes,
-    update_supplier_data,
-)
+from managers.hospital_manager import (retrieve_hospital,
+                                       retrieve_hospital_codes,
+                                       update_hospital_data)
+from managers.supplier_manager import (get_supplier_info,
+                                       retrieve_supplier_codes,
+                                       update_supplier_data)
 from utils.cli import clear_screen, go_back, select_from_list
-from utils.text_manipulation import convert_to_snake_case, get_between_parantheses
+from utils.text_manipulation import (convert_to_snake_case,
+                                     get_between_parantheses)
 from utils.textfiles_database import load_data
 
 
@@ -36,20 +33,20 @@ def update_details_handler(controller):
 
     if category_selection == category_choices[0]:
         supplier_codes = retrieve_supplier_codes()
-        hospital_choices = list(
+        supplier_choices = list(
             map(
                 lambda x: get_supplier_info(x)["supplier_company_name"] + f" ({x})",
                 supplier_codes,
             )
         )
 
-        unparsed_hospital_selection = select_from_list(
-            "Which supplier do you want to edit?", hospital_choices
+        unparsed_supplier_selection = select_from_list(
+            "Which supplier do you want to edit?", supplier_choices
         )
-        if unparsed_hospital_selection == "Back":
+        if unparsed_supplier_selection == "Back":
             return go_back(controller)
-        hospital_selection = get_between_parantheses(unparsed_hospital_selection)[0]
-        hospital = get_supplier_info(hospital_selection)
+        supplier_selection = get_between_parantheses(unparsed_supplier_selection)[0]
+        supplier = get_supplier_info(supplier_selection)
 
         attribute_choices = [
             "Supplier Person Name",
@@ -66,29 +63,29 @@ def update_details_handler(controller):
             f"What do you want to change the {unparsed_attribute_selection} to >> "
         )
 
-        hospital[attribute_selection] = value_input
-        update_supplier_data(hospital_selection, hospital)
+        supplier[attribute_selection] = value_input
+        update_supplier_data(supplier_selection, supplier)
         clear_screen()
 
         print(
-            f"[Supplier Handler]: Updated {hospital_selection}'s {unparsed_attribute_selection} to {value_input}"
+            f"[Supplier Handler]: Updated {supplier_selection}'s {unparsed_attribute_selection} to {value_input}"
         )
     elif category_selection == category_choices[1]:
         hospital_codes = retrieve_hospital_codes()
-        hospital_choices = list(
+        supplier_choices = list(
             map(
                 lambda x: retrieve_hospital(x)["hospital_name"] + f" ({x})",
                 hospital_codes,
             )
         )
 
-        unparsed_hospital_selection = select_from_list(
-            "Which supplier do you want to edit?", hospital_choices
+        unparsed_supplier_selection = select_from_list(
+            "Which supplier do you want to edit?", supplier_choices
         )
-        if unparsed_hospital_selection == "Back":
+        if unparsed_supplier_selection == "Back":
             return go_back(controller)
-        hospital_selection = get_between_parantheses(unparsed_hospital_selection)[0]
-        hospital = retrieve_hospital(hospital_selection)
+        supplier_selection = get_between_parantheses(unparsed_supplier_selection)[0]
+        supplier = retrieve_hospital(supplier_selection)
 
         attribute_choices = [
             "Hospital Name",
@@ -104,12 +101,12 @@ def update_details_handler(controller):
             f"What do you want to change the {unparsed_attribute_selection} to >> "
         )
 
-        hospital[attribute_selection] = value_input
-        update_hospital_data(hospital_selection, hospital)
+        supplier[attribute_selection] = value_input
+        update_hospital_data(supplier_selection, supplier)
         clear_screen()
 
         print(
-            f"[Hospital Handler]: Updated {hospital_selection}'s {unparsed_attribute_selection} to {value_input}"
+            f"[Hospital Handler]: Updated {supplier_selection}'s {unparsed_attribute_selection} to {value_input}"
         )
     elif category_selection == "Back":
         return go_back(controller)
